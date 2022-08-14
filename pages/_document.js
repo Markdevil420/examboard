630d2a3c203369eb73aafe8f9c3a654aa606fe4f
Markdevil420 <98700463+Markdevil420@users.noreply.github.com>
2022-08-14 21:29:55 +0530

    add google adsense code

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,7 @@ class MyDocument extends Document {
         <body>
             <Main/>
             <script async src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"></script>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2675825941309382" crossorigin="anonymous"></script>
             <NextScript />
         </body>
       </Html>
